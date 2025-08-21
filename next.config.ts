2cfb35c144ab31e956928084a97e5e408db97fe8
gabri <gabrielc.cavalcante@hotmail.com>
2025-08-20 22:35:17 -0300

    ignore eslint

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+   eslint: {
+    ignoreDuringBuilds: true, // <--- ignora ESLint no build
+  },
 };
 
 export default nextConfig;
